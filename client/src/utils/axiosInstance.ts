58c7dda75ed88f1f7b51bf6a6fa0ee86de538772
rushikeshmore154 <rushikeshvmore1524@gmail.com>
2025-10-02 17:57:06 +0530

    production

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://intellihire-backend-imgk.onrender.com",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://intellihire-backend-imgk.onrender.com",
   withCredentials: true, // if you need cookies
 });
 
